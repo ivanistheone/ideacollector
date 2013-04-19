5287ebdbac0be7ca471a49e48c66b8d2d6406b9e
Ivan Savov <ivan.savov@gmail.com>
2013-04-18 23:40:53 -0400

    Ok NOOOOW its for real

---
+++ js/questions.js
@@ -0,0 +1,31 @@
+// The quesitons data for Idea Collector script
+// [Ivan Savov](http://cs.mcgill.ca/~isavov/). 
+
+
+// Load the application once the DOM is ready, using `jQuery.ready`:
+
+App.questions_data = {
+    "business":  [      "How will you make money?",
+                        "What problem are you solving?",
+                        "Will people be willing to pay for your solution?",
+                        "How much money can you make? Really?",
+                        "What is your competitive advantage?",
+                        "Will you want to work on this in 1y, 2y, 5y?",
+                        "Is your business good for humanity?",
+                        "Give a 3 sentence sales pitch that will work on your grandmother."    ],
+    "community":    [   "Why will people join the project?",
+                        "What will people get from the project?",
+                        "What will people contribute?",
+                        "Why do YOU want to start this project?",
+                        "How will the project be funded?",
+                        "How will you deal with problematic people?",
+                        "Will you want to work on this in 1y, 2y, 5y?",
+                        "Has it been done before?" ],
+    "bblcok":       [   "Previous technology",
+                        "New technology",
+                        "Advantages",
+                        "Technical details",
+                        "Next steps" ]
+    };
+
+
